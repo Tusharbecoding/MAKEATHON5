91b6b59c931dddc0ca613a2748817449581601b7
Samik Malhotra <samikmalhotra66@gmail.com>
2023-02-26 00:45:56 +0530

    wip: campaign controller

---
+++ backend/controllers/campaignController.js
@@ -0,0 +1,25 @@
+import asyncHandler from "express-async-handler";
+import Campaign from "../models/campaignModel";
+
+// @desc create a new campaign
+// @route POST /api/campaigns
+// @access Private
+const createCampaign = asyncHandler(async(req, res)=>{
+
+})
+
+// @desc get all campaigns
+// @route GET /api/campaigns
+// @access Public
+
+// @desc get campaign by id
+// @route GET /api/campaign/:id
+// @access Public
+
+// @desc 
+// @route
+// @access
+
+// @desc 
+// @route
+// @access
